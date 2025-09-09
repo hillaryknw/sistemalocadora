d9b0d659dd1b7efd4ba97757651db6e304f939a5
hy <hillarycarvalho2510@gmail.com>
2025-09-09 14:03:52 -0300

    mais classes foram adicionadas

--- classe.py
+++ classe.py
@@ -20,3 +20,32 @@ class Item:
     def returnTape(self): #Return a tape if it's not avaiable 
         if not self.__status:
             self.__status = True
+
+
+# -----------------------------------------------------------
+
+class Film(Item):
+    def __init__(self, code, title, genre, duration):
+        super().__init__(code, title)
+        self.__genre = genre
+        self.__duration = duration
+
+class Game(Item):
+    def __init__(self, code, title, platform, age_range):
+        super().__init__(code, title)
+        self.__platform = platform
+        self.__age_range = age_range
+
+# -----------------------------------------------------------
+
+class Costumer():
+    def __init__(self, name, cpf):
+        self.__name = name
+        self.__cpf = cpf
+        self.__rented_items = []
+
+    def alocateItem(self, item: Item):
+        self.__rented_items.append(item)
+
+    def returnItem(self, item: Item):
+        self.__rented_items.pop(item)
